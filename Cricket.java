e09124dbf91898afa911b60269d3a15669abccf1
mohsinkhan994 <mkhan5332@gmail.com>
2021-08-11 15:30:39 +0530

    awsdkjv

--- Cricket.java
+++ Cricket.java
@@ -1,9 +1,16 @@
-public class Cricket { //implements Ability {
+public class Cricket {
 	
 	public static void main(String args[])
 	{
 		String name="Mohsin";
-		
+		String lastname= "Khan";
+
+		String lstname= "Khan";
+wadfjkgb
+asdfg
+
+wejrthklkflkadaeriue5uanrntkakut
+
 		
 	}
 
